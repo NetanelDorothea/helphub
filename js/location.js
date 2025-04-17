d0877f8b788e1178f4a771e0f9f538a6c4067c19
NetanelDorothea <42834387+NetanelDorothea@users.noreply.github.com>
2025-04-17 17:17:11 -0500

    Add files via upload

--- js/location.js
+++ js/location.js
@@ -1,11 +1,19 @@
 // fetch('https://api.miip.my')
-fetch('http://ip-api.com/json/')
-        .then(res => res.json() )
-        .then(data => {
-                // alert(JSON.stringify(data)) 
-                if (data.status === "success") {
-                        document.getElementById("location").innerHTML = `Charities near <strong>${data.city}, ${data.region}</strong>`
-                } else {
-                        document.getElementById("location").innerHTML = `We weren't able to retrieve your location.`
+// fetch('https://members.ip-api.com/')
+fetch('https://freeipapi.com/api/json/')
+        .then(res => {
+                if (!res.ok){
+                        throw new Error ("Network response was not ok");
+                }else{
+                        return res.json()
                 }
+        })
+        .then(data => {
+                // alert(JSON.stringify(data))
+                document.getElementById("location").innerHTML = `Charities near <strong>${data.cityName}, ${data.regionName}</strong>`
+        })
+        .catch(error => {
+                console.error('Fetch error:', error);
+                document.getElementById("location").innerHTML =
+                        `We weren't able to retrieve your location.`;
         });
